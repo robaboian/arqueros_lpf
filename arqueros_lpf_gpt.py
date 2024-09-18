1eac92cb3dc4ffc85021145bfb2645639354adac
robaboian <robaboian@gmail.com>
2024-09-18 01:39:43 -0300

    Update arqueros_lpf_gpt.py

--- arqueros_lpf_gpt.py
+++ arqueros_lpf_gpt.py
@@ -197,14 +197,18 @@ def main():
     st.write("###### Referencias:")
     st.write("###### PSxG: Índice de expectativa de gol post-remate. Referencia a la peligrosidad de los remates al arco recibidos. | Goles evitados: Se calcula por la diferencia entre los goles en contra y el PSxG.")
 
-    st.subheader("Estadísticas de los arqueros estandarizadas cada 90'.")
-    st.dataframe(arqueros90)
-    
     st.markdown(
     """
     Me gustaría agradecer a [Lanus Stats](https://www.youtube.com/@LanusStats/videos) y a [McKay Johns](https://www.youtube.com/@McKayJohns) por sus aportes a la comunidad.
-    """
-)
+    """)
+    
+    st.subheader("Estadísticas de los arqueros estandarizadas cada 90'.")
+    st.dataframe(arqueros90)
+
+    st.write("###### X: @robaboian_")
+
+    
+    
 
 
 
